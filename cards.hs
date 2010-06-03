2b9069e694d3185b4689efbc756a7322aecd5b6f
Øyvind Grønnesby <oyving@pvv.org>
2010-06-03 02:50:16 +0200

    Made Card enumerable

--- cards.hs
+++ cards.hs
@@ -1,12 +1,12 @@
--- Simple cards utility modules written in Haskell, all for fun
+    -- Simple cards utility modules written in Haskell, all for fun
 module Cards where
 import System.Random
 
 data Suit = Club | Diamond | Heart | Spade
-	deriving (Eq, Ord, Show, Read, Enum)
+	deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
-	deriving (Eq, Ord, Show, Read, Enum)
+	deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
 data Card = Card Suit Rank
 	deriving (Show, Read)
@@ -15,13 +15,27 @@ instance Eq Card where
 	Card sa ra == Card sb rb = (sa == sb) && (ra == rb)
 
 instance Ord Card where
-	Card sa ra < Card sb rb = sa < sb || (sa == sb && ra < rb)
+    (Card sa ra) < (Card sb rb) | ra < rb = True
+                                | sa < sb = True
+                                | otherwise = False
+
+instance Bounded Card where
+	minBound = Card Club Two
+	maxBound = Card Spade Ace
+
+instance Enum Card where
+    fromEnum (Card s r) = (fromEnum r) * 4 + (fromEnum s)
+    toEnum a            =
+        Card (toEnum x) (toEnum y)
+        where
+            x = a `mod` 4
+            y = (a - x) `div` 4
 
 type Deck = [Card]
 
 makeDeck :: Deck
-makeDeck = [ Card suit rank | suit <- [Club .. Spade], rank <- [Two, Three .. Ace] ]
-
+makeDeck = [minBound .. maxBound]
+    
 shuffle :: (RandomGen r) => r -> [x] -> [x]
 shuffle gen a = shuffle' gen a []
 	where
